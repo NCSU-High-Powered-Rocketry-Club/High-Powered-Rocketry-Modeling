995baecd05918a5d8b765a0c6f968e5613c7f4fe
jgelia <jgelia@ncsu.edu>
2026-06-20 01:38:53 -0700

    bump version number and add x86_64 linux to wheel scripts

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 # Run this script to compile the library for different python versions and platforms:
 
-# Will build for x86_64:
+# Native host builds:
 uv run -p 3.14 -- maturin build --release -i 3.14t --compatibility pypi
 uv run -p 3.14 -- maturin build --release -i 3.14 --compatibility pypi
 uv run -p 3.14 -- maturin build --release -i 3.13 --compatibility pypi
@@ -8,7 +8,15 @@ uv run -p 3.14 -- maturin build --release -i 3.12 --compatibility pypi
 uv run -p 3.14 -- maturin build --release -i 3.11 --compatibility pypi
 uv run -p 3.14 -- maturin build --release -i 3.10 --compatibility pypi
 
-# Will build for aarch64:
+# Will build for x86_64 linux (requires zig):
+uv run -p 3.14 -- maturin build --release -i 3.14t --compatibility pypi --target x86_64-unknown-linux-gnu --zig
+uv run -p 3.14 -- maturin build --release -i 3.14 --compatibility pypi --target x86_64-unknown-linux-gnu --zig
+uv run -p 3.14 -- maturin build --release -i 3.13 --compatibility pypi --target x86_64-unknown-linux-gnu --zig
+uv run -p 3.14 -- maturin build --release -i 3.12 --compatibility pypi --target x86_64-unknown-linux-gnu --zig
+uv run -p 3.14 -- maturin build --release -i 3.11 --compatibility pypi --target x86_64-unknown-linux-gnu --zig
+uv run -p 3.14 -- maturin build --release -i 3.10 --compatibility pypi --target x86_64-unknown-linux-gnu --zig
+
+# Will build for aarch64 linux (requires zig):
 uv run -p 3.14 -- maturin build --release -i 3.14t --compatibility pypi --target aarch64-unknown-linux-gnu --zig
 uv run -p 3.14 -- maturin build --release -i 3.14 --compatibility pypi --target aarch64-unknown-linux-gnu --zig
 uv run -p 3.14 -- maturin build --release -i 3.13 --compatibility pypi --target aarch64-unknown-linux-gnu --zig
